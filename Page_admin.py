4ba588e3a49e0947aa31014f293aaeba4ffd502d
HumYacom <thainakdee06@hotmail.com>
2023-01-20 16:18:05 +0700

    v7

--- Page_admin.py
+++ Page_admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect,session
+from flask import Blueprint, render_template, request, url_for, redirect,session,flash
 import pymysql
 from dbconnect import *
 from flask_paginate import Pagination, get_page_args
@@ -84,3 +84,122 @@ def Admin_del():
             return redirect(url_for('Document_products.Admin_index'))
 
     return redirect(url_for('Document_products.Admin_index'))
+
+#menu products
+@Document_products.route("/pd")
+def pd():
+    with db.cursor() as cur:
+        sql = "SELECT Products.Product_id, Products.Product_type, inventory.Product_category, Products.Product_manner, Products.Product_quantity,Products.Product_date,inventory.type_id,inventory.Product_status FROM Products INNER JOIN inventory ON Products.Product_type = inventory.Product_type;"
+        try:
+            cur.execute(sql)
+            db.commit()
+        except:
+            return render_template('admin/menu_pd.html', datas=('nodata'))
+        rows = cur.fetchall()
+
+        return render_template('admin/menu_pd.html', datas=rows)
+
+@Document_products.route("/PD_edit", methods=["POST"])
+def PD_edit():
+    if request.method == "POST":
+        runum = request.form['Product_id']
+        re_pstatus = request.form['Product_type']
+        Product_type = request.form['Product_manner']
+        re_unit = request.form['Product_price']
+        re_status = request.form['type_id']
+        Product_status = request.form['Product_status']
+        Product_category = request.form['Product_category']
+        with db.cursor() as cur:
+            sql = "UPDATE Products SET Product_type = %s, Product_manner = %s, Product_quantity = %s, type_id = %s WHERE Product_id = %s"
+            sql2 = "UPDATE inventory SET Product_type = %s, Product_category = %s, Product_status = %s WHERE type_id = %s"
+            try:
+                cur.execute(sql, (re_pstatus, Product_type, re_unit, re_status, runum))
+                db.commit()
+                cur.execute(sql2, (re_status, re_pstatus, Product_status, Product_category))
+                db.commit()
+            except:
+                return redirect(url_for('Document_products.pd'))
+
+            return redirect(url_for('Document_products.pd'))
+
+    return redirect(url_for('Document_products.pd'))
+
+
+@Document_products.route("/PD_del", methods=["POST"])
+def PD_del():
+    if request.method == "POST":
+        No = request.form['Product_id']
+        with db.cursor() as cur:
+            sql = "DELETE FROM inventory WHERE type_id = %s"
+            sql2 = "DELETE FROM Products WHERE Product_id = %s"
+            try:
+                cur.execute(sql,(No))
+                db.commit()
+                cur.execute(sql2,(No))
+                db.commit()
+            except:
+                return redirect(url_for('Document_products.pd'))
+
+            return redirect(url_for('Document_products.pd'))
+
+    return redirect(url_for('Document_products.pd'))
+
+@Document_products.route("/PDadding")
+def PDadding():
+    return render_template("admin/PD_add.html")
+
+
+@Document_products.route("/PDAdd", methods=["POST"])
+def PDAdd():
+    if request.method == "POST":
+        runum = request.form['Product_id']
+        re_pstatus = request.form['Product_type']
+        Product_type = request.form['Product_manner']
+        re_unit = request.form['Product_price']
+        re_date = request.form['Product_date']
+        re_status = request.form['type_id']
+        Product_status = request.form['Product_status']
+        Product_category = request.form['Product_category']
+        with db.cursor() as cur:
+            sql = "INSERT INTO inventory(type_id,Product_type,Product_category,Product_status) VALUES(%s,%s,%s,%s)"
+            sql2 = "INSERT INTO products(Product_id,Product_type,Product_manner,Product_quantity,Product_date,type_id) VALUES(%s,%s,%s,%s,%s,%s)"
+            try:
+                cur.execute(sql,(re_status,re_pstatus,Product_category,Product_status))
+                db.commit()
+                cur.execute(sql2,(runum,re_pstatus,Product_type,re_unit,re_date,re_status))
+                db.commit()
+                flash(
+                    "ได้ทำการส่งเรื่องขอวัสดุแล้วกำลังส่งข้อมูล...กรุณารอสักครู่ครับ")
+            except:
+                return redirect(url_for('Document_products.pd', status="wait"))
+
+            return redirect(url_for('Document_products.pd', status="wait"))
+
+    return render_template("admin/PD_add.html", status="wait")
+
+#menu rigister products
+@Document_products.route("/menurigis")
+def menurigis():
+    with db.cursor() as cur:
+        sql = "SELECT * FROM inventory"
+        try:
+            cur.execute(sql)
+            db.commit()
+        except:
+            return render_template('admin/menu_pdregis.html', datas=('nodata'))
+        rows = cur.fetchall()
+        return render_template('admin/menu_pdregis.html', datas=rows)
+
+#menu broken products
+
+@Document_products.route("/menubroken")
+def menubroken():
+    with db.cursor() as cur:
+        sql = "SELECT * FROM products_broken"
+        try:
+            cur.execute(sql)
+            db.commit()
+        except:
+            return render_template('admin/menu_pdbroken.html', datas=('nodata'))
+        rows = cur.fetchall()
+        return render_template('admin/menu_pdbroken.html', datas=rows)
--- Page_detail.py
+++ Page_detail.py
@@ -8,12 +8,14 @@ db = pymysql.connect(host=HOST, user=USER, password=PASS, database=DATABASE)
 detailpd = Blueprint('detailpd', __name__)
 
 
-@detailpd.route("/detail_product")
+@detailpd.route("/detail_product",methods = ["GET"])
 def detail_product():
+    args = request.args
+    data = args.get('no')
     with db.cursor() as cur:
-        sql = "SELECT requisition.re_no, requisition.User_name, requisition.re_pstatus, requisition.Product_type, requisition.re_unit,Products.Product_manner, requisition.re_date,inventory.type_id,requisition.re_status FROM ((Products INNER JOIN inventory ON Products.Product_type = inventory.Product_type) INNER JOIN requisition ON Products.Product_type = requisition.Product_type) WHERE requisition.User_name;"
+        sql = "SELECT requisition.re_no, requisition.User_name, requisition.re_pstatus, requisition.Product_type, requisition.re_unit,Products.Product_manner, requisition.re_date,inventory.type_id,requisition.re_status FROM ((Products INNER JOIN inventory ON Products.Product_type = inventory.Product_type) INNER JOIN requisition ON Products.Product_type = requisition.Product_type) WHERE requisition.re_no = %s;"
         try:
-            cur.execute(sql)
+            cur.execute(sql,(data))
             db.commit()
         except:
             return render_template('detail/detailPD.html', datas=('nodata'))
